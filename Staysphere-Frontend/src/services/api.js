39a5918335518a901be634d5cbbcfbc39bfc5c1e
k_mahesh <konda.mahesh1250@gmail.com>
2026-05-11 18:30:24 +0530

    api

--- Staysphere-Frontend/src/services/api.js
+++ Staysphere-Frontend/src/services/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import.meta.env.VITE_API_BASE_URL
+// import.meta.env.VITE_API_BASE_URL
+
 const API = axios.create({
   baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
 });
